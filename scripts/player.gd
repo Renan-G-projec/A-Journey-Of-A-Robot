5c9468b79bcdd6cc104b1071be85ada1cae297fe
Renan-G-projec <renanfartes@gmail.com>
2026-09-10 08:00:49 -0300

    feat: Jetpack basic scaffolding added. AMDG

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,30 @@
+# Ad Maiorem Dei Gloriam!
+class_name Player
 extends CharacterBody2D
-
 
 @export var SPEED: float = 100.0
 @export var JUMP_VELOCITY: float = -400.0
+
+# Jetpack settings
+const jetpack_max_fuel: float = 100.0
+var jetpack_current_fuel: float = 100.0
+var jetpack_consume_rate: float = 10.0
+var jetpack_impulse: float = 40.0
+var jetpack_max_velocity: float = 230.0 
+var jetpack_is_active: bool = false
 
 
 func _physics_process(delta: float) -> void:
     if not is_on_floor():
         velocity += get_gravity() * delta
 
-    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+    jetpack_is_active = Input.is_action_pressed("UseJetpack") && jetpack_current_fuel > 0
+    if jetpack_is_active:
+        jetpack_current_fuel -= jetpack_consume_rate * delta
+        print(velocity.y)
+        velocity.y -= jetpack_impulse;
+        velocity.y = max(velocity.y, -jetpack_max_velocity)
+        
 
     var direction := Input.get_axis("ui_left", "ui_right")
     if direction:
