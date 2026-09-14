406a4725dc9a95bd971737f5139f3a4ea6fd6795
Renan-G-projec <renanfartes@gmail.com>
2026-09-14 19:49:04 -0300

    feat: Added basic item resource interface. AMDG

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -3,9 +3,15 @@ extends Node2D
 @onready var map: Map = $Map
 @onready var resource_ui: Control = $GameUI/ResourceUI
 
+func _ready() -> void:
+    EventBus.inventory_changed.connect(_on_inventory_changed)
+
 func _on_player_fuel_changed(new_fuel: float) -> void:
     resource_ui.set_fuel(new_fuel)
 
 
 func _on_player_mined_block(tilemap_coords: Vector2i, damage: float) -> void:
     map.damage_tile(tilemap_coords, damage)
+
+func _on_inventory_changed(item: InventoryItem, new_qtd: int) -> void:
+    print("ITEM ADDED");
