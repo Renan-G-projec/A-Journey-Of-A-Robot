3db4e32fe50e3453d917af4b3882a5c739145721
Renan-G-projec <renanfartes@gmail.com>
2026-09-15 09:39:55 -0300

    fix: Connect all the generated Tiles. AMDG

--- scripts/driil.gd
+++ scripts/driil.gd
@@ -26,67 +26,67 @@ const facing_block_animation_time: float = 0.3
 signal mined_block(tilemap_position: Vector2i, damage: float)
 
 func _physics_process(delta: float) -> void:
-	var mouse_pos: Vector2 = get_global_mouse_position()
-	var mouse_ang: float = global_position.angle_to_point(mouse_pos)
-	sprite.rotation = mouse_ang
-	
-	var ray_vector: Vector2 = (range * 16 * Vector2.from_angle(mouse_ang))
-	raycast.target_position = ray_vector
-	
-	var changed_facing_block: bool
-	var collider: TileMapLayer = raycast.get_collider() as TileMapLayer
-	if collider:
-		var collision_point: Vector2 = collider.to_local(raycast.get_collision_point() + Vector2.from_angle(mouse_ang) * 0.1)
-		
-		var collision_tile: Vector2i = collider.local_to_map(collision_point)
-		
-		changed_facing_block = collision_tile != facing_block_coords
-		
-		is_facing_block = true;
-		facing_block_coords = collision_tile
-		
-		var global_tile_position: Vector2 = collider.to_global(collider.map_to_local(collision_tile))
-		selected_block_effect.global_position = global_tile_position
+    var mouse_pos: Vector2 = get_global_mouse_position()
+    var mouse_ang: float = global_position.angle_to_point(mouse_pos)
+    sprite.rotation = mouse_ang
+    
+    var ray_vector: Vector2 = (range * 16 * Vector2.from_angle(mouse_ang))
+    raycast.target_position = ray_vector
+    
+    var changed_facing_block: bool
+    var collider: TileMapLayer = raycast.get_collider() as TileMapLayer
+    if collider:
+        var collision_point: Vector2 = collider.to_local(raycast.get_collision_point() + Vector2.from_angle(mouse_ang) * 0.1)
+        
+        var collision_tile: Vector2i = collider.local_to_map(collision_point)
+        
+        changed_facing_block = collision_tile != facing_block_coords
+        
+        is_facing_block = true;
+        facing_block_coords = collision_tile
+        
+        var global_tile_position: Vector2 = collider.to_global(collider.map_to_local(collision_tile))
+        selected_block_effect.global_position = global_tile_position
 
-		# Here it updates in advance. Does not start emitting
-		particles.global_position = global_tile_position
-		
-	else:
-		is_facing_block = false;
-	
-	update_selected_block_effect(changed_facing_block)
-	update_timers(delta)
+        # Here it updates in advance. Does not start emitting
+        particles.global_position = global_tile_position
+        
+    else:
+        is_facing_block = false;
+    
+    update_selected_block_effect(changed_facing_block)
+    update_timers(delta)
 
 func mine() -> void:
-	if (is_facing_block): 
-		mined_block.emit(facing_block_coords, damage)
-		if (facing_block_animation_timer < 0): 
-			selected_block_effect.queue_redraw()
-			update_selected_block_effect(true, Vector2(0.7, 0.7))
-			facing_block_animation_timer = facing_block_animation_time
+    if (is_facing_block): 
+        mined_block.emit(facing_block_coords, damage)
+        if (facing_block_animation_timer < 0): 
+            selected_block_effect.queue_redraw()
+            update_selected_block_effect(true, Vector2(0.7, 0.7))
+            facing_block_animation_timer = facing_block_animation_time
 
 func update_selected_block_effect(reset_lerp: bool, effect_scale: Vector2 = select_lerp_effect, step: float = select_lerp_effect_step) -> void:
-	selected_block_effect.visible = is_facing_block
-	if (!is_facing_block): return
-	if (reset_lerp):
-		selected_block_effect.scale = effect_scale
-	selected_block_effect.scale.x = lerp(selected_block_effect.scale.x, 1.0, step)
-	selected_block_effect.scale.y = lerp(selected_block_effect.scale.y, 1.0, step)
-	
+    selected_block_effect.visible = is_facing_block
+    if (!is_facing_block): return
+    if (reset_lerp):
+        selected_block_effect.scale = effect_scale
+    selected_block_effect.scale.x = lerp(selected_block_effect.scale.x, 1.0, step)
+    selected_block_effect.scale.y = lerp(selected_block_effect.scale.y, 1.0, step)
+    
 func update_timers(delta: float) -> void:
-	facing_block_animation_timer -= delta
-	
+    facing_block_animation_timer -= delta
+    
 func start_emitting_particles() -> void:
-	particles.emitting = true;
+    particles.emitting = true;
 
 func _process(_delta: float) -> void:
-	if particles.emitting:
-		var process_material: ShaderMaterial = particles.process_material as ShaderMaterial
-		if process_material:
-			process_material.set_shader_parameter("targetPos", sprite.global_position)
-			process_material.set_shader_parameter("initial_velocity", Vector2.from_angle(deg_to_rad(randi_range(0, 360))) * 48)
-			process_material.set_shader_parameter("initial_offset", Vector2(randi_range(-8, 8), randi_range(-4, 4)))
+    if particles.emitting:
+        var process_material: ShaderMaterial = particles.process_material as ShaderMaterial
+        if process_material:
+            process_material.set_shader_parameter("targetPos", sprite.global_position)
+            process_material.set_shader_parameter("initial_velocity", Vector2.from_angle(deg_to_rad(randi_range(0, 360))) * 48)
+            process_material.set_shader_parameter("initial_offset", Vector2(randi_range(-8, 8), randi_range(-4, 4)))
 
 func stop_emitting_particles() -> void:
-	particles.emitting = false
-		
+    particles.emitting = false
+        
--- scripts/map.gd
+++ scripts/map.gd
@@ -4,46 +4,47 @@ extends Node2D
 
 @onready var layer: TileMapLayer = $PlanetLayer1
 
-
 @export var tile_base_life: float = 100.0
 var tiles_life: Dictionary[Vector2i, float] = {}
 
 func damage_tile(tile: Vector2i, damage: float) -> void:
-	var life: float = tiles_life.get_or_add(tile, tile_base_life)
-	if life - damage <= 0:
-		destroy_tile(tile)
-	else:
-		tiles_life[tile] -= damage
-	
+    var life: float = tiles_life.get_or_add(tile, tile_base_life)
+    if life - damage <= 0:
+        destroy_tile(tile)
+    else:
+        tiles_life[tile] -= damage
+    
 func destroy_tile(tile: Vector2i) -> void:
-	layer.set_cells_terrain_connect([tile], 0, -1)
-	tiles_life.erase(tile)
-	
-	
+    layer.set_cells_terrain_connect([tile], 0, -1)
+    tiles_life.erase(tile)
+    
+    
 func _ready() -> void:
-	layer.clear()
-	var world_width: int = 30
-	var layer_1_height: int = 15
-	
-	var map_index_height: Array = []
-	var map_index_skim: Array = []
-	
-	for i in range (world_width): 
-		var random_height_int: int = randi_range(5, layer_1_height) 
-		map_index_height.append(random_height_int)
-		var random_width_skim: int = randi_range(0, 3) 
-		map_index_skim.append(random_width_skim)
-	
-	for i in range (world_width):
-		var y_skim: int = map_index_skim[i]
-		var column_height: int = map_index_height[i]
-		
-		for j in range(y_skim, column_height):
-			var grid_position: Vector2i = Vector2i(i, j+15) 
-			var source_id: int = 0
-			var atlas_coords: Vector2i = Vector2i(1,1)
-			layer.set_cell(grid_position, source_id, atlas_coords)
-			
-	
-	
-	
+    layer.clear()
+    var world_width: int = 30
+    var layer_1_height: int = 15
+    
+    var map_index_height: Array = []
+    var map_index_skim: Array = []
+    
+    var positions_to_change: Array[Vector2i] = []
+    
+    for i in range (world_width): 
+        var random_height_int: int = randi_range(5, layer_1_height) 
+        map_index_height.append(random_height_int)
+        var random_width_skim: int = randi_range(0, 3) 
+        map_index_skim.append(random_width_skim)
+    
+    for i in range (world_width):
+        var y_skim: int = map_index_skim[i]
+        var column_height: int = map_index_height[i]
+        
+        for j in range(y_skim, column_height):
+            var grid_position: Vector2i = Vector2i(i, j+15)
+            positions_to_change.push_back(grid_position)
+            
+    layer.set_cells_terrain_connect(positions_to_change, 0, 0)
+            
+    
+    
+    
