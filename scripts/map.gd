3df6767cc2f146208ce3f898e25e4ca83b9e1e73
HuntOrBeHunted83 <learn.ishank.ramnath@gmail.com>
2026-09-14 20:43:07 -0500

    More changes to map gen

--- scripts/map.gd
+++ scripts/map.gd
@@ -25,18 +25,25 @@ func _ready() -> void:
 	var world_width: int = 30
 	var layer_1_height: int = 15
 	
+	var map_index_height: Array = []
+	var map_index_skim: Array = []
 	
-	var map_index: Array = []
 	for i in range (world_width): 
-		var random_int: int = randi_range(5, layer_1_height) 
-		map_index.append(random_int)
+		var random_height_int: int = randi_range(5, layer_1_height) 
+		map_index_height.append(random_height_int)
+		var random_width_skim: int = randi_range(0, 3) 
+		map_index_skim.append(random_width_skim)
 	
 	for i in range (world_width):
-		for j in range(map_index[i-1]):
+		var y_skim: int = map_index_skim[i]
+		var column_height: int = map_index_height[i]
+		
+		for j in range(y_skim, column_height):
 			var grid_position: Vector2i = Vector2i(i, j+15) 
 			var source_id: int = 0
-			var atlass: Vector2i = Vector2i(1,1)
-			layer.set_cell(grid_position, source_id, atlass)
+			var atlas_coords: Vector2i = Vector2i(1,1)
+			layer.set_cell(grid_position, source_id, atlas_coords)
+			
 	
 	
 	
