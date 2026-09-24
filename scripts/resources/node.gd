d7d1acaa70b02fc406d98edc86ad413f2e13572a
HuntOrBeHunted83 <learn.ishank.ramnath@gmail.com>
2026-09-24 09:26:50 -0500

    Got Tech Tree Popup working

--- scripts/resources/node.gd
+++ scripts/resources/node.gd
@@ -7,10 +7,10 @@ var delay := 10
 
 func add_line(neighbor: Button) -> void:
 	var line: Line2D = Line2D.new()
-	line.width = 2
+	line.width = 4
 	line.z_index = -1
-	line.add_point(position + size / 2)
-	line.add_point(neighbor.position + neighbor.size/2)
+	line.add_point(global_position + size / 2)
+	line.add_point(neighbor.global_position + neighbor.size/2)
 	
 	lines.push_back(line)
 	neighbor.neighbor_lines.push_back(line)
@@ -18,28 +18,28 @@ func add_line(neighbor: Button) -> void:
 
 func update_lines() -> void:
 	for line: Line2D in lines:
-		line.set_point_position(0, position + size/2)
+		line.set_point_position(0, global_position + size/2)
 	for line: Line2D in neighbor_lines:
-		line.set_point_postion(1, position + size/2)
+		line.set_point_position(1, global_position + size / 2)
 
 func set_neighbors (new_neighbors: Array) -> void:	
 	for neighbor: Button in new_neighbors:
-		if not neighbor.has(neighbor):
+		if not neighbors.has(neighbor):
 			add_line(neighbor)
 	neighbors = new_neighbors
 func _on_Button_toggled(button_pressed: Button) -> void:
-	if pressed and button_pressed:
+	if button_pressed:
 		for neigbor: Button in neighbors:
 			neigbor.disabled = false
 			
 
 func _process(delta: float) -> void:	
-	if Input.is_action_just_pressed("mouse_left") and is_hovered():
+	if Input.is_action_just_pressed("ui_left") and is_hovered():
 		delay = delay - 1
 		if delay < 0:
 			position = get_global_mouse_position() - size/2
 			update_lines()
-	if Input.is_action_just_released("mouse_left"):
+	if Input.is_action_just_released("ui_left"):
 		delay = 10
 		
 		
